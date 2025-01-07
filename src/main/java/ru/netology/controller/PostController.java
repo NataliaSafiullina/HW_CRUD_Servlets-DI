7d64b23fab0a60a601b3b87738f6927f280d65a7
NataliaSafiullina <nfsafiullina@gmail.com>
2025-01-07 21:38:10 +0700

    Add PostRepository methods

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 
 public class PostController {
   public static final String APPLICATION_JSON = "application/json";
+  public static final String PLAIN_TEXT = "text/plain";
   private final PostService service;
 
   public PostController(PostService service) {
@@ -17,25 +18,44 @@ public class PostController {
   }
 
   public void all(HttpServletResponse response) throws IOException {
+    // Устанавливаем тип контента ответа в формате JSON
     response.setContentType(APPLICATION_JSON);
+    // Получаем список всех постов
     final var data = service.all();
+    // Создаем объект Gson для сериализации данных
     final var gson = new Gson();
+    // Отдаем список постов в формате JSON
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public void getById(long id, HttpServletResponse response) throws IOException {
+    // Устанавливаем тип контента ответа в формате JSON
+    response.setContentType(APPLICATION_JSON);
+    // Создаем объект Gson для сериализации данных
+    final var gson = new Gson();
+    // Отдаем найденный пост в формате JSON
+    response.getWriter().print(gson.toJson(service.getById(id)));
   }
 
   public void save(Reader body, HttpServletResponse response) throws IOException {
+    // Устанавливаем тип контента ответа в формате JSON
     response.setContentType(APPLICATION_JSON);
+    // Создаем объект Gson для десериализации данных
     final var gson = new Gson();
+    // Десериализуем тело запроса в объект Post
     final var post = gson.fromJson(body, Post.class);
+    // Сохраняем пост в базе данных и получаем обратно то, что сохранили
     final var data = service.save(post);
+    // Вернем в качестве ответа JSON сохраненный пост
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public void removeById(long id, HttpServletResponse response) throws IOException {
+   // Устанавливаем тип контента ответа в формате постой текст
+    response.setContentType(PLAIN_TEXT);
+    // Удаляем пост по id и получаем результат операции
+    final var result = service.removeById(id);
+    // Отдадим результат операции в качестве ответа
+    response.getWriter().print(result);
   }
 }
--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,75 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
+
+  protected AtomicLong counter = new AtomicLong();
+  protected ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+
+  /**
+   * Получить все посты
+   * @return - список постов
+   */
   public List<Post> all() {
-    return Collections.emptyList();
+      return new ArrayList<>(posts.values());
   }
 
+  /**
+   * Получить пост по id
+   * @param id - id поста
+   * @return - пост
+   */
   public Optional<Post> getById(long id) {
-    return Optional.empty();
+    return Optional.ofNullable(posts.get(id));
   }
 
+  /**
+   * Сохранение поста в хранилище
+   * @param post - получаем пост
+   * @return - сохраненный пост
+   */
   public Post save(Post post) {
+    if (post == null ) {
+      throw new IllegalArgumentException("post must not be null");
+    }
+    // Если от клиента приходит пост с id=0, значит, это создание нового поста.
+    if (post.getId() == 0) {
+      post.setId(counter.incrementAndGet());
+      posts.put(post.getId(), post);
+    }
+    // Если от клиента приходит пост с id<>0, значит, это обновление уже существующего поста.
+    else {
+      if (posts.containsKey(post.getId())) {
+        posts.put(post.getId(), post);
+      }
+      else {
+        throw new IllegalArgumentException("post id must be exist");
+      }
+    }
     return post;
   }
 
-  public void removeById(long id) {
+  /**
+   * Удаление поста по ID
+   * @param id - id поста
+   * @return - сообщение об успешном удалении поста или не удаленном
+   */
+  public String removeById(long id) {
+    // Метод remove() возвращает null, если такой записи нет,
+    // или возвращает бывшее значение связанное с ключом, если запись была
+    if (posts.remove(id) != null) {
+      return "Post with id = " + id + " removed";
+    } else {
+      return "Post with id = " + id + " not found";
+    }
   }
+
 }
